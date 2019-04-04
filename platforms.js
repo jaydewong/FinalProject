e7e392e1ea8c0fca4a984d1ea8a6c93388b4deae
Jayde Wong <2002jaydewong@gmail.com>
2019-04-03 17:30:54 -0700

    attempting background

---
+++ platforms.js
@@ -0,0 +1,27 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', 'assets/background.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+}
+
+function update ()
+{
+}
