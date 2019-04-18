fdb6dce6ca8f75950b75bffcb09e14d3db614682
HyperSpectrum <44418167+HyperSpectrum@users.noreply.github.com>
2019-04-17 18:01:00 -0700

    Added Coins in Final

--- platforms.js
+++ platforms.js
@@ -22,6 +22,7 @@ var game = new Phaser.Game(config);
 var anim;
 var progress;
 var frameView;
+var coins;
 var platforms;
 var player;
 var cursors;
@@ -32,12 +33,14 @@ function preload ()
     this.load.image('sky', 'background.png');
     this.load.image('ground', 'fixedground.png');
     this.load.spritesheet('player', 'Pusheen1.png', { frameWidth: 46, frameHeight: 28 });
+    this.load.spritesheet('coin', 'Coin1.png', { frameWidth: 13, frameHeight: 13 });
 }
 
 function updateFrameView ()
 {
     frameView.clear();
     frameView.fillRect(player.frame.cutX, 0, 46, 28);
+    frameView.fillRect(coins.frame.cutX, 0, 13, 13);
 }
 
 function update ()
@@ -51,11 +54,16 @@ function create ()
 {   
     this.add.image(400, 300, 'sky').setScale(3);
     platforms = this.physics.add.staticGroup();
+    coins = this.physics.add.staticGroup();
 
-    platforms.create(400, 568, 'ground').setScale(4).refreshBody();
+    platforms.create(400, 560, 'ground').setScale(4).refreshBody();
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+    coins.create(400, 420, 'coin').setScale(1.5).refreshBody();;
+    coins.create(600, 340, 'coin').setScale(1.5).refreshBody();;
+    coins.create(50, 190, 'coin').setScale(1.5).refreshBody();;
+    coins.create(750, 160, 'coin').setScale(1.5).refreshBody();;
 
     player = this.physics.add.sprite(100,300,'player');
     player.setBounce(0.2);
@@ -65,7 +73,7 @@ function create ()
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player, platforms,);
 
     this.anims.create({
         key: 'walk',
@@ -80,6 +88,16 @@ function create ()
         frames: [{ key: 'player', frame: 0}],
         frameRate: 18,
     });
+
+    this.anims.create({
+        key: 'spin',
+        frames: this.anims.generateFrameNumbers('coin'),
+        frameRate: 10,
+        yoyo: true,
+        repeat: -1
+    });
+
+    coins.playAnimation('spin');
 }
 
 function generatePlatforms(){
